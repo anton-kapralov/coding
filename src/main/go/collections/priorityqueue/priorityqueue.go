d6fbdf9a38a44f5f5b436cf944bc10773e4a130e
Anton Kapralov <a.e.kapralov@gmail.com>
2024-01-27 10:57:52 -0800

    [go/collections] Implement PriorityQueue

---
+++ src/main/go/collections/priorityqueue/priorityqueue.go
@@ -0,0 +1,56 @@
+package priorityqueue
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type PriorityQueue[T any] interface {
+	heap.Interface
+	Peek() T
+}
+
+type priorityQueue[T any] struct {
+	items []T
+	less  func(a, b T) bool
+}
+
+func (pq *priorityQueue[T]) Peek() T {
+	return pq.items[len(pq.items)-1]
+}
+
+func (pq *priorityQueue[T]) Push(x any) {
+	item := x.(T)
+	pq.items = append(pq.items, item)
+}
+
+func (pq *priorityQueue[T]) Pop() any {
+	n := len(pq.items)
+	item := pq.items[n-1]
+	var zero T
+	pq.items[n-1] = zero
+	pq.items = pq.items[:n-1]
+	return item
+}
+
+func (pq *priorityQueue[T]) Len() int {
+	return len(pq.items)
+}
+
+func (pq *priorityQueue[T]) Less(i, j int) bool {
+	return !pq.less(pq.items[i], pq.items[j])
+}
+
+func (pq *priorityQueue[T]) Swap(i, j int) {
+	pq.items[i], pq.items[j] = pq.items[j], pq.items[i]
+}
+
+func (pq *priorityQueue[T]) String() string {
+	return fmt.Sprint(pq.items)
+}
+
+func New[T any](less func(a, b T) bool) PriorityQueue[T] {
+	return &priorityQueue[T]{
+		less: less,
+	}
+}
